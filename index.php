94b92d9d733d6bfe8496c1784fb6861bd39de85b
albertpark <albert_park@hotmail.com>
2020-04-30 10:34:55 -0400

    Updating to current

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?
 
-echo 'albertpark.me';
+require('define.php');
+include_once(__ROOT__ . DS . 'library' . DS . 'helper.php');
+
+// client-side controlled value
+// echo $_SERVER[HTTP_HOST];
+// server-side controlled value
+// echo $_SERVER[SERVER_NAME];
+
+#debug($_SERVER);
+
+print '<pre>';
+echo($_SERVER[REQUEST_SCHEME] . '://' . $_SERVER[SERVER_NAME] . $_SERVER[REQUEST_URI]);
+print '</pre>';
 
 ?>
